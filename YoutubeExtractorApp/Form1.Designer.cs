6c6e9e22a615c2014c307dc91a9a8d1d6592ec4d
sinuee Hernandez <sinuee@gmail.com>
2017-01-17 16:53:55 -0600

    All the code

---
+++ YoutubeExtractorApp/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace YoutubeExtractorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Url = new System.Windows.Forms.TextBox();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.console = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressFile = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(12, 32);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(504, 199);
+            this.list.TabIndex = 2;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(357, 4);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Url
+            // 
+            this.Url.Location = new System.Drawing.Point(12, 6);
+            this.Url.Name = "Url";
+            this.Url.Size = new System.Drawing.Size(339, 20);
+            this.Url.TabIndex = 4;
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 286);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(504, 23);
+            this.progress.TabIndex = 5;
+            // 
+            // console
+            // 
+            this.console.AutoSize = true;
+            this.console.Location = new System.Drawing.Point(9, 270);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(35, 13);
+            this.console.TabIndex = 6;
+            this.console.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(441, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // progressFile
+            // 
+            this.progressFile.Location = new System.Drawing.Point(12, 333);
+            this.progressFile.Name = "progressFile";
+            this.progressFile.Size = new System.Drawing.Size(504, 23);
+            this.progressFile.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 366);
+            this.Controls.Add(this.progressFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.Url);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "YoutubeExtractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox Url;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label console;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressFile;
+    }
+}
+
---
+++ YoutubeExtractorApp/Form1.cs
@@ -0,0 +1,188 @@
+﻿using NAudio.Lame;
+using NAudio.MediaFoundation;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YoutubeExtractorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Our test youtube link
+            //list.Items.Add("https://www.youtube.com/watch?v=-befR4wHsjQ&index=1&list=PLn07dXvOHuupuzoM92qfw4Zf0tis-8vFr");
+            progressFile.Value = 0;
+            progressFile.Maximum = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progress.Value = 0;
+            progress.Maximum = list.Items.Count;
+            progress.Update();
+            foreach (var item in list.Items)
+            {
+                /*
+                 * Get the available video formats.
+                 * We'll work with them in the video and audio download examples.
+                 */
+                IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(item.ToString(), false);
+                string fileName= "";
+                console.Text = "Procesing " + progress.Value+1 + " of " + progress.Maximum+ ": " + item;
+                try
+                {
+                    DownloadAudio(videoInfos, out fileName);
+                    //DownloadVideo(videoInfos, out fileName);
+                }
+                catch (Exception ex)
+                {
+                    console.Text = fileName + ": " + ex.Message;
+                }
+                progress.Value++;
+                progress.Update();
+            }
+
+        }
+
+
+        private  void DownloadAudio(IEnumerable<VideoInfo> videoInfos, out string fileName)
+        {
+            /*
+             * We want the first extractable video with the highest audio quality.
+             */
+            VideoInfo video = videoInfos
+                .Where(info => info.CanExtractAudio)
+                .OrderByDescending(info => info.AudioBitrate)
+                .FirstOrDefault();
+
+            if (video == null)
+            {
+                //throw new Exception("Can not extract audio");
+                DownloadVideo(videoInfos, out fileName);
+                AlternativeAudioExtraction(fileName);
+                return;
+            }
+
+            /*
+             * If the video has a decrypted signature, decipher it
+             */
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            /*
+             * Create the audio downloader.
+             * The first argument is the video where the audio should be extracted from.
+             * The second argument is the path to save the audio file.
+             */
+
+            fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                RemoveIllegalPathCharacters(video.Title) + video.AudioExtension);
+            var audioDownloader = new AudioDownloader(video, fileName);
+
+            // Register the progress events. We treat the download progress as 85% of the progress
+            // and the extraction progress only as 15% of the progress, because the download will
+            // take much longer than the audio extraction.
+            audioDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 0.85);
+            audioDownloader.AudioExtractionProgressChanged += (sender, args) => Console.WriteLine(85 + args.ProgressPercentage * 0.15);
+
+            /*
+             * Execute the audio downloader.
+             * For GUI applications note, that this method runs synchronously.
+             */
+            audioDownloader.Execute();
+        }
+
+        private void DownloadVideo(IEnumerable<VideoInfo> videoInfos, out string fileName)
+        {
+            progressFile.Value = 0;
+            /*
+             * Select the first .mp4 video with 360p resolution
+             */
+            VideoInfo video = videoInfos
+                .FirstOrDefault(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+
+            /*
+             * If the video has a decrypted signature, decipher it
+             */
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            /*
+             * Create the video downloader.
+             * The first argument is the video to download.
+             * The second argument is the path to save the video file.
+             */
+            fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                RemoveIllegalPathCharacters(video.Title) + video.VideoExtension);
+            label1.Text = fileName;
+            var videoDownloader = new VideoDownloader(video, fileName);
+
+            // Register the ProgressChanged event and print the current progress
+            //videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
+            videoDownloader.DownloadProgressChanged += (sender, args) => progressFile.Value = (int)args.ProgressPercentage;
+            progressFile.Update();
+
+            /*
+             * Execute the video downloader.
+             * For GUI applications note, that this method runs synchronously.
+             */
+            videoDownloader.Execute();
+        }
+
+
+        private  string RemoveIllegalPathCharacters(string path)
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
+        }
+
+        public  void AlternativeAudioExtraction(string path)
+        {
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(path);
+            using (var reader = new MediaFoundationReader(path))
+            {
+
+
+                var outFile = Path.Combine(fileInfo.DirectoryName, fileInfo.Name.Replace(fileInfo.Extension, ".wav"));
+                WaveFileWriter.CreateWaveFile(outFile, reader);
+                using (var waveReader = new WaveFileReader(outFile))
+                {
+                    using (var wtr = new LameMP3FileWriter(outFile.Replace(".wav", ".mp3"), waveReader.WaveFormat, 128))
+                    {
+                        waveReader.CopyTo(wtr);
+                        wtr.Close();
+                        //MediaFoundationEncoder.EncodeToMp3(waveReader, outFile.Replace(".wav", ".mp3"));                    
+                    }
+                }
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            list.Items.Add(Url.Text);
+            Url.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Items.Remove(list.Items[list.SelectedIndex]);
+        }
+    }
+}
